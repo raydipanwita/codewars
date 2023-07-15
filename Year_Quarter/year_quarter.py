bfe5e1fae220d544bb8bae03dcaa904227bfad8c
Dipanwita (Dipa) Ray <58883688+raydipanwita@users.noreply.github.com>
2023-07-15 15:54:42 +0100

    Year_Quarter

---
+++ Year_Quarter/year_quarter.py
@@ -0,0 +1,11 @@
+def quarter_of(month):
+    # your code here
+    if month <= 3:
+        return 1
+    elif month <= 6:
+        return 2
+    elif month <= 9:
+        return 3
+    else:
+        return 4
+    pass
